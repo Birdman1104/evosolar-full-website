ec33e5d486b3f6b5508cb078feed5514a52f8554
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-02-18 13:20:58 +0400

    Change for test.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="app">
     <Header />
-    <Services />
+    <!-- <Services />
     <BlocksSection />
     <OfferSection />
     <CalculatorButton />
-    <CalculatorSection />
+    <CalculatorSection /> -->
   </div>
 </template>
 
 <script>
 import Header from './components/HeaderComponent.vue';
-import Services from './components/ServicesComponent.vue';
-import BlocksSection from './components/BlocksSection.vue';
-import OfferSection from './components/OfferSection.vue';
-import CalculatorButton from './components/CalculatorButton.vue';
-import CalculatorSection from './components/CalculatorSection.vue';
+// import Services from './components/ServicesComponent.vue';
+// import BlocksSection from './components/BlocksSection.vue';
+// import OfferSection from './components/OfferSection.vue';
+// import CalculatorButton from './components/CalculatorButton.vue';
+// import CalculatorSection from './components/CalculatorSection.vue';
 
 export default {
   components: {
     Header,
-    Services,
-    BlocksSection,
-    OfferSection,
-    CalculatorButton,
-    CalculatorSection,
+    // Services,
+    // BlocksSection,
+    // OfferSection,
+    // CalculatorButton,
+    // CalculatorSection,
   },
 };
 </script>
