a69ac0d1c409356fdb248ab817f90d85aa074cac
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-02-21 19:36:19 +0400

    Fixes in offer section.

--- src/components/OfferSection.vue
+++ src/components/OfferSection.vue
@@ -3,9 +3,11 @@
     <h2>5 STEPS TO YOUR SOLAR SYSTEM</h2>
     <p>Explore our services and solutions.</p>
     <div class="circles">
-      <div class="circle" v-for="(offer, index) in offers" :key="index">
-        <div class="icon">{{ offer.icon }}</div>
-        <h3>{{ offer.title }}</h3>
+      <div class="circle-wrapper" v-for="(offer, index) in offers" :key="index">
+        <div class="circle">
+          <div class="icon">{{ offer.icon }}</div>
+        </div>
+        <h3 v-html="offer.title"></h3>
       </div>
     </div>
   </section>
@@ -16,7 +18,7 @@ export default {
   data() {
     return {
       offers: [
-        { title: 'Consultation, Measurement', icon: '💡' },
+        { title: 'Consultation,<br>Measurement', icon: '💡' },
         { title: 'Design', icon: '🎨' },
         { title: 'Preparation Work', icon: '📋' },
         { title: 'Installation', icon: '🔧' },
@@ -39,35 +41,48 @@ export default {
   justify-content: space-around;
   align-items: center;
   position: relative;
+  margin-top: 30px;
 }
 
 .circles::before {
   content: '';
   position: absolute;
-  top: 50%;
+  top: 35%;
   left: 0;
   right: 0;
   height: 2px;
-  background: black;
+  background: #193D27;
   z-index: 0;
 }
 
-.circle {
+.circle-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   position: relative;
   z-index: 1;
+  color: #193D27;
+}
+
+.circle {
   background: white;
-  border: 2px solid black;
+  border: 2px solid #193D27;
   border-radius: 50%;
   width: 100px;
   height: 100px;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-bottom: 10px;
 }
 
 .icon {
   font-size: 24px;
+}
+
+h3 {
+  margin: 0;
+  font-size: 1rem;
 }
 
 @media (max-width: 768px) {
